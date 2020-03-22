f385d16d4233c3ac4e23312023881e8124c656e9
Hp <Hp@192.168.0.178>
2020-03-22 12:09:10 +0100

    fuub

--- testling/Testus.java
+++ testling/Testus.java
@@ -1,9 +1,18 @@
 package testling;
 
+import java.sql.SQLException;
+
 public class Testus {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
+		jdbc db = new jdbc();
+		
+		db.getInfected();
+		
+		
+		
+		db.close();
 	}
 
 }
