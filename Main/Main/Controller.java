119e49441e4ed575c42f6865c6951fb862a00530
Hp <Hp@192.168.0.178>
2020-04-09 11:45:02 +0200

    neues layout

--- Main/Main/Controller.java
+++ Main/Main/Controller.java
@@ -1,61 +1,57 @@
 package Main;
 
-
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-
-import javax.print.DocFlavor.URL;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
+
+	Mysql mq;
 	
+
 	ObservableList list = FXCollections.observableArrayList();
 	@FXML
 	private ListView<String> countryList;
-	private Mysql mq;
-	Label Data = new Label();
+ 
+
+	@Override
+	public void initialize(java.net.URL arg0, ResourceBundle arg1) {
+
+		
+		try {
+			addList();
+		} catch (ClassNotFoundException | SQLException e) {
+
+			e.printStackTrace();
+		}
+
+	}
 
 	public void loginButtonClicked() {
 
 		System.out.println("User logged in");
 	}
-	
 
-	public void addList() throws ClassNotFoundException, SQLException{
+	public void addList() throws ClassNotFoundException, SQLException {
 		mq = new Mysql();
+
 		
+
 		list.removeAll(list);
-		
-		
+
 		ArrayList countries = mq.getCountry();
 		for (int i = 0; i < countries.size(); i++) {
-			
+
 			list.addAll(countries.get(i));
 		}
-		
-		
-	//	list.addAll("World","China","Italy","Spain","Germany","Iran","USA","France","S. Korea","Switzerland","UK","Netherlands","Belgium","Austria","Norway","Sweden","Denmark","Portugal","Malaysia","Canada","Australia","Brazil","Japan","Czechia","Israel","Pakistan","Diamond Princess","Ireland","Turkey","Luxembourg","Chile","Finland","Greece","Iceland","Qatar","Poland","Indonesia","Singapore","Ecuador","Thailand","Saudi Arabia","Slovenia","Romania","Philippines","Russia","Estonia","Bahrain","Egypt","India","Hong Kong","Peru","South Africa","Iraq","Lebanon","Croatia","Mexico","Panama","Kuwait","Bulgaria","Armenia","Argentina","Colombia","Taiwan","UAE","San Marino","Serbia","Slovakia","Latvia","Costa Rica","Dominican Republic","Uruguay","Hungary","Algeria","Vietnam","Bosnia and Herzegovina","Faeroe Islands","Andorra","Morocco","Jordan","North Macedonia","Cyprus","Lithuania","Brunei","Moldova","Sri Lanka","Albania","Belarus","Malta","Venezuela","Burkina Faso","Tunisia","Azerbaijan","Cambodia","Kazakhstan","New Zealand","Oman","Palestine","Guadeloupe","Georgia","Trinidad and Tobago","Senegal","Réunion","Ukraine","Uzbekistan","Liechtenstein","Martinique","Cameroon","Bangladesh","Afghanistan","Honduras","DRC","Nigeria","Cuba","Jamaica","Bolivia","Ghana","Paraguay","Macao","Puerto Rico","Rwanda","Togo","French Guiana","French Polynesia","Guam","Mauritius","Channel Islands","Ivory Coast","Kyrgyzstan","Montenegro","Guatemala","Maldives","Monaco","Gibraltar","Mongolia","Ethiopia","Kenya","Mayotte","Seychelles","Barbados","Equatorial Guinea","Tanzania","U.S. Virgin Islands","Guyana","Aruba","Gabon","Bahamas","New Caledonia","Saint Martin","Suriname","Cayman Islands","Curaçao","Cabo Verde","CAR","Congo","El Salvador","Liberia","Madagascar","Namibia","St. Barth","Zimbabwe","Sudan","Angola","Benin","Bermuda","Bhutan","Fiji","Greenland","Guinea","Haiti","Isle of Man","Mauritania","Nicaragua","Saint Lucia","Zambia","Nepal","Antigua and Barbuda","Chad","Djibouti","Gambia","Vatican City","Montserrat","Niger","Papua New Guinea","St. Vincent Grenadines","Sint Maarten","Somalia","Eswatini","Timor-Leste");
-		countryList.getItems().addAll(list);
-		
-	}
 
-	@Override
-	public void initialize(java.net.URL arg0, ResourceBundle arg1) {
-		
-		try {
-			addList();
-		} catch (ClassNotFoundException | SQLException e) {
-			
-			e.printStackTrace();
-		}
+		countryList.getItems().addAll(list);
+
 	}
 
 }
--- Main/Main/Mysql.java
+++ Main/Main/Mysql.java
@@ -8,61 +8,65 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-public class Mysql {
 
-	private Connection con;
+public class Mysql{
+	
+private Connection con;
 
-	public Mysql() throws ClassNotFoundException, SQLException {
+public Mysql() throws ClassNotFoundException, SQLException {
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		con = DriverManager.getConnection(
-				"jdbc:mysql://localhost/Corona?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
-				"root", "12345");
+	Class.forName("com.mysql.jdbc.Driver");
+	 con = DriverManager.getConnection(
+			"jdbc:mysql://localhost/Corona?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+			"root", "12345");
 
+}
+
+public ArrayList<String> getCountry() throws SQLException {
+
+	ArrayList<String> countr = new ArrayList<>();
+	PreparedStatement stmt;
+	ResultSet rs;
+
+	String Name;
+	String format;
+	String confirmedCases;
+	int confirmed = 0;
+	String sql1 = "select cases  from Corona limit 187";
+	stmt = con.prepareStatement(sql1);
+	rs = stmt.executeQuery();
+	while(rs.next())
+	{
+		confirmed = confirmed + rs.getInt("cases");
 	}
+	Name = "World";
+	confirmedCases = Integer.toString(confirmed);
+	format = String.format("%-30s \t %s", Name, confirmedCases);
+	String sql = "select country, cases from Corona limit 187;";
+	countr.add(format);
+	stmt = con.prepareStatement(sql);
 
-	public ArrayList<String> getCountry() throws SQLException {
-
-		ArrayList<String> countr = new ArrayList<>();
-		PreparedStatement stmt;
-		ResultSet rs;
-
-		String Name;
-		String format;
-		String confirmedCases;
-		int confirmed;
-		String sql1 = "select sum(cases)  from Corona limit 187";
-		stmt = con.prepareStatement(sql1);
-		rs = stmt.executeQuery();
-		rs.next();
-		confirmed = rs.getInt("sum(cases)");
-		Name = "World";
-		confirmedCases = Integer.toString(confirmed);
+	// stmt.setString(1,ss);
+	rs = stmt.executeQuery();
+	while (rs.next()) {
+		Name = rs.getString("Country");
+		confirmed = rs.getInt("cases");
+		confirmedCases = String.valueOf(confirmed);
 		format = String.format("%-30s \t %s", Name, confirmedCases);
-		String sql = "select country, cases from Corona limit 187;";
 		countr.add(format);
-		stmt = con.prepareStatement(sql);
-
-		// stmt.setString(1,ss);
-		rs = stmt.executeQuery();
-		while (rs.next()) {
-			Name = rs.getString("Country");
-			confirmed = rs.getInt("cases");
-			confirmedCases = String.valueOf(confirmed);
-			format = String.format("%-30s \t %s", Name, confirmedCases);
-			countr.add(format);
-		}
-
-		stmt.close();
-		rs.close();
-		// return countr;
-		System.out.println(countr);
-		return countr;
-
 	}
 
-	public void close() throws SQLException {
-		// TODO Auto-generated method stub
-		con.close();
-	}
+	stmt.close();
+	rs.close();
+	// return countr;
+	System.out.println(countr);
+	return countr;
+
+}
+
+
+public void close() throws SQLException {
+	// TODO Auto-generated method stub
+	con.close();
+}
 }
