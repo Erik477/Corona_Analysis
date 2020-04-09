0af8de6e2df961b7198eff7af28b88810bcf026d
Erik <Erik@DESKTOP-DHUULHB.fritz.box>
2020-04-09 10:35:58 +0200

    errors fixxen

--- Main/Main/Controller.java
+++ Main/Main/Controller.java
@@ -3,33 +3,36 @@ package Main;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-import javax.print.DocFlavor.URL;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 
 public class Controller implements Initializable {
 
 	Mysql mq;
+	ListView<String> Data;
 
 	ObservableList list = FXCollections.observableArrayList();
 	@FXML
 	private ListView<String> countryList;
-	private ListView<String> Data;
+ 
 
 	@Override
 	public void initialize(java.net.URL arg0, ResourceBundle arg1) {
 
+		Data = new ListView<>();
+		
+		Data.getItems().addAll("Info","Cases", "todayCases", "Deaths", "todayDeaths", "recovered", "critical","CasesPerOneMillion");		
+		
 		try {
 			addList();
 		} catch (ClassNotFoundException | SQLException e) {
 
 			e.printStackTrace();
 		}
+
 	}
 
 	public void loginButtonClicked() {
@@ -40,8 +43,7 @@ public class Controller implements Initializable {
 	public void addList() throws ClassNotFoundException, SQLException {
 		mq = new Mysql();
 
-		Data.getItems().addAll("Cases", "todayCases", "Deaths", "todayDeaths", "recovered", "critical",
-				"CasesPerOneMillion");
+		
 
 		list.removeAll(list);
 
